bac31b06d8cb880ed1ea47ce8ce76c0d0d3206cb
RCJU\sdi232 <sebastien.chevre@jura.ch>
2021-11-24 08:20:37 +0100

    configure start script

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const port = process.env.PORT | 3000
 
 var connectedUsers = [];
 
